d6710cb0b38ed083d7ac1da0c8041469641954c8
Hanna Kurdzel <gannanychyporchuk@gmail.com>
2017-12-15 16:35:06 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('.buttons button').forEach( el => {
+    el.addEventListener('click', switchTab);
+});
+
+function switchTab(ev) {
+    document.querySelectorAll('.buttons button, .tabs div').forEach( el => {
+        el.classList.remove('active');
+    });
+    ev.target.classList.add('active');
+    const tab = ev.target.dataset.tab;
+    document.querySelectorAll('.tabs div').forEach( el => {
+        if (el.dataset.tab === tab ) {
+            el.classList.add('active');
+        }
+    })
+}
+
+$(function(){
+   $('#contact-form input[type=submit]').click(sendForm); 
+});
+
+function sendForm(ev) {
+    const form  = document.getElementsByTagName('form')[0];
+    if (form.checkValidity()) {
+        ev.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/gannanychyporchuk@gmail.com", 
+            method: "POST",
+            data: {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                feedback: $('#feedback').val()
+            },
+            dataType: "json"
+        })
+         .done( () => $('#thank-dialog').attr('open', 'open') )
+         .fail( () => $('#error-dialog').attr('open', 'open') );    
+    }
+}
